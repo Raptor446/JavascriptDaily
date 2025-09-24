ad2f3a239f75af8309dbb3ec7a97be4943c1ae4c
ayush jaiswal <ayushjaiswal326@gmail.com>
2025-09-24 23:51:06 +0530

    Daily commit

--- InterviewQuestions/Week3/generateBinaryNumbers.js
+++ InterviewQuestions/Week3/generateBinaryNumbers.js
@@ -6,19 +6,19 @@ Append x + "0" and x + "1".
 Continue until N numbers are generated.
 */
 function generateBinaryNumbers(n) {
-  let q = ["1"];
-  let result = [];
+  let q = ["1"]
+  let result = []
 
   while (n--) {
-    let front = q.shift();
-    result.push(front);
+    let front = q.shift()
+    result.push(front)
 
-    q.push(front + "0");
-    q.push(front + "1");
+    q.push(front + "0")
+    q.push(front + "1")
   }
 
-  return result;
+  return result
 }
 
-console.log(generateBinaryNumbers(5));
+console.log(generateBinaryNumbers(5))
 // ["1", "10", "11", "100", "101"]
