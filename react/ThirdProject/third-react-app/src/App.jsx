ec2fe211561ba83730c4b8a5193305014fadda29
ayush jaiswal <ayushjaiswal326@gmail.com>
2025-11-27 21:20:11 +0530

    Daily commit

--- react/ThirdProject/third-react-app/src/App.jsx
+++ react/ThirdProject/third-react-app/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import Greeting from './Greetings.jsx'
 import './App.css'
 
 function App() {
@@ -28,7 +27,6 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-        <Greeting />
       </p>
     </>
   )
